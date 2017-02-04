7dc52ebe6bacdb0fe5061f77755a03a5b1c04b56
Guto Rocha <guto@gutorocha.com>
2017-02-04 19:17:49 -0200

    Montando a estrutura de diretórios

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+
+gulp.task('serve', function () {
+
+    browserSync.init({
+        server: "app"
+    });
+
+    gulp.watch('app/pages/*.html', function () {
+        browserSync.reload();
+    });
+
+});
+
+gulp.task('build', ['serve']);
