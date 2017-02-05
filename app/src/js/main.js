8540f3c22af8ec1ff16783a0604fd490894b4b13
Guto Rocha <guto@gutorocha.com>
2017-02-05 08:44:46 -0200

    Limpeza de arquivos

--- app/src/js/main.js
+++ app/src/js/main.js
@@ -2,8 +2,6 @@ var angular = require('angular');
 var uiRouter = require('angular-ui-router');
 var Chart = require('chart.js');
 
-var URL_API = 'https://autoscalegenerator.herokuapp.com';
-
 var appEscalaTricall = angular.module('appEscalaTricall', [uiRouter]);
 
 appEscalaTricall.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
@@ -65,68 +63,6 @@ appEscalaTricall.config(['$stateProvider', '$urlRouterProvider', function ($stat
         controller: 'GeracaoEscalaController',
         controllerAs: 'vm'
     });
-
-}]);
-
-appEscalaTricall.factory('AuthenticationService', ['$window', function ($window) {
-
-    var usuarioTeste = {
-        username: 'admin@admin.com.br',
-        password: 'admin'
-    };
-
-    var tokenTeste = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9.TJVA95OrM7E2cBab30RMHrHDcEfxjoYZgeFONFh7HgQ';
-    var service = {};
-
-    service.login = function (username, password, callback) {
-        if (usuarioTeste.username === username && usuarioTeste.password === password) {
-            $window.localStorage.setItem('appUsername', username);
-            $window.localStorage.setItem('appToken', tokenTeste);
-
-            if (!angular.isUndefined(callback)) {
-                callback(true);
-            }
-
-        } else {
-
-            if (!angular.isUndefined(callback)) {
-                callback(false);
-            }
-
-        }
-    };
-
-    service.logout = function () {
-        $window.localStorage.removeItem('appUsername');
-        $window.localStorage.removeItem('appToken');
-    };
-
-    service.auth = function (callback) {
-        if (usuarioTeste.username !== $window.localStorage.getItem('appUsername') && tokenTeste !== $window.localStorage.getItem('appToken')) {
-            service.logout();
-
-            if (!angular.isUndefined(callback)) {
-                callback();
-            }
-        }
-    };
-
-    service.getUsuarioTeste = function () {
-        return usuarioTeste;
-    };
-
-    return service;
-
-}]);
-
-appEscalaTricall.controller('SidebarController', ['$location', function ($location) {
-
-    var vm = this;
-
-    vm.isActive = function (viewLocation) {
-        console.log($location.path());
-        return (viewLocation === $location.path());
-    };
 
 }]);
 
@@ -428,60 +364,6 @@ appEscalaTricall.controller('ParametroSistemaController', [function () {
     };
 
     vm.lista();
-
-}]);
-
-appEscalaTricall.controller('LoginController', ['$location', 'AuthenticationService', function ($location, AuthenticationService) {
-
-    var vm = this;
-
-    vm.login = function () {
-
-        vm.enviando = true;
-        vm.enviandoMensagem = 'Enviando...';
-        vm.erro = false;
-        vm.erroMensagem = 'Usuário ou senha incorretos.';
-
-        AuthenticationService.login(vm.username, vm.password, function (result) {
-            if (result === true) {
-                $location.path('/contatos');
-            } else {
-                vm.enviando = false;
-                vm.erro = true;
-            }
-        });
-
-    };
-
-}]);
-
-appEscalaTricall.controller('LogoutController', ['$location', 'AuthenticationService', function ($location, AuthenticationService) {
-
-    var vm = this;
-
-    vm.logout = function () {
-        AuthenticationService.logout();
-        $location.path('/');
-    };
-
-}]);
-
-appEscalaTricall.controller('EsqueciMinhaSenhaController', ['AuthenticationService', function (AuthenticationService) {
-
-    var vm = this;
-    var usernameAtual = AuthenticationService.getUsuarioTeste();
-
-    vm.esqueciminhasenha = function () {
-
-        if (angular.equals({username: vm.username}, {username: usernameAtual.username})) {
-            vm.alertType = "alert-success";
-            vm.mensagem = "Você receberá uma nova senha em seu e-mail de cadastro.";
-        } else {
-            vm.alertType = "alert-warning";
-            vm.mensagem = "Cadastro não encontrado em nosso sistema.";
-        }
-
-    }
 
 }]);
 
